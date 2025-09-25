50f77b17fba009f8248244e7448e361859bb4a32
gauravborse1210 <gauravborse1210@gmail.com>
2025-09-25 12:27:34 +0530

    Cleaning code

--- src/index.js
+++ src/index.js
@@ -115,12 +115,6 @@ function Footer() {
   const isOpen = hour >= openHour && hour <= closeHour;
   console.log(isOpen);
 
-  // if (hour >= openHour && hour <= closeHour) {
-  //   alert("We're currently open!");
-  // } else {
-  //   alert("Sorry we're closed");
-  // }
-
   return (
     <footer className="footer">
       {isOpen ? (
